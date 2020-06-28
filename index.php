7e64a8749b613f0e0ebf7047fabaa6d11e051f3b
kudo <kuasehujikolp@yahoo.co.jp>
2020-06-28 23:49:23 +0900

    a

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>トップ｜漫画</title>
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+<link href="style.css?<?= time() ?>" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+
+
+</body>
+</html>
